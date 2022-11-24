7151c66f9d3f37e00b84b69c7a3a8ba6f998a4e1
ahmathelte <ahmed.a.b.homoudi@gmx.de>
2022-11-24 15:23:55 +0100

    clima_abs

--- R/clima_diagramm_abs.R
+++ R/clima_diagramm_abs.R
@@ -5,19 +5,71 @@
 #' '/production/Prod_klima_diagramm_4corners.R'. The input data are representing grids
 #' in the KlimaKonform model region or at least Regions. All meta data required to plot this are either
 #' the production script or the function here.
-#' @param data A data frame containing the daily data to be plotted. The data should contain
-#' min, max, and mean temperature and precipitation. For only three locations, e.g., MO or Greiz.
+#'
+#' @param data A data frame containing the data to be plotted. The data should contain
+#' min, max, and mean temperature and precipitation.
+#' @param temp_precip_mean A vector of the long term average of temperature and pürecipition
 #' @param run_id A character variable either pointing out the run id, please refer
 #'  to the simulation setup file.
-#' @param output_path A string pointing to the output directory for output plot
+#' @param output_path A string pointing to the output directory for output plot.
+#'
 #' @author Ahmed Homoudi
-#' @return  PNG
+#' @return  ggplot2 plot
 #' @import stats
 #' @importFrom utils globalVariables write.table
 #' @export
 #' @seealso  clima_diagramm_change
+#'
 clima_diagramm_abs <- function(data,
-                               run_id,
-                               output_path) {
-  zoo::as.Date()
+                               temp_precip_mean,
+                               run_id) {
+
+
+
+  # change to month
+
+  data$MONTH<-month.abb[data$MONTH]
+  data$MONTH<-factor(data$MONTH, levels = month.abb)
+
+
+
+
+  # plot
+
+  ylim.prim <- c(-5,50)   # in this example, temperature
+  ylim.sec <- c(-10,100)    # in this example, precipitation
+
+  b <- diff(ylim.prim)/diff(ylim.sec)
+  a <- ylim.prim[1] - b*ylim.sec[1] # there was a bug here
+
+ggplot(climate, aes(Month, Precip)) +
+  geom_col() +
+  geom_line(aes(y = a + Temp*b), color = "red") +
+  scale_y_continuous("Precipitation", sec.axis = sec_axis(~ (. - a)/b, name = "Temperature")) +
+  scale_x_continuous("Month", breaks = 1:12) +
+  ggtitle("Climatogram for Oslo (1961-1990)")
+
+
+  unique(data$Period)
+
+  ggplot2::ggplot(data = data)+
+    ggplot2::geom_col(mapping = ggplot2::aes(x = MONTH,
+                                             y = Precip))+
+    ggplot2::geom_path(mapping = ggplot2::aes(x = MONTH,
+                                              y = Tmin,
+                                              group=1),
+                       linetype = "dotted")+
+    ggplot2::geom_path(mapping = ggplot2::aes(x = MONTH,
+                                              y = Tavg,
+                                              group=1),
+                       linetype = "solid")+
+    ggplot2::geom_path(mapping = ggplot2::aes(x = MONTH,
+                                              y = Tmax,
+                                              group=1),
+                       linetype = "dashed")+
+
+    ggplot2::scale_y_continuous(limits = ylim.prim,
+                                sec.axis = ggplot2::sec_axis(~ (. - a)/b, name = "precipitation") )
+
+
 }
--- production/Prod_klima_diagramm_4corners.R
+++ production/Prod_klima_diagramm_4corners.R
@@ -2,7 +2,7 @@
 library(KlimaKonformC3)
 
 dirs<-list.dirs(path = "/media/ahmed/Daten/WHK2/Data/climate", full.names = T, recursive = F)
-
+dirs<-list.dirs(path = "D:/AHomoudi/KlimaKonform/5ter_Lauf_2022-10-28", full.names = T, recursive = F)
 idir =1
 for(idir in 1:length(dirs)){
 
@@ -83,9 +83,135 @@ for(idir in 1:length(dirs)){
   corners<-unique(klima_df$corners)
 
   for( icorner in 1:length(corners)){
+
     #icorner<-1
     sub_df<-klima_df%>%dplyr::filter(corners==corners[icorner])
 
+    # calculate monthly mean values of Precip, Tmin, Tavg, and Tmean for each month
+
+
+    data_mon<-sub_df%>%
+      dplyr::mutate(yearmonth = zoo::as.yearmon(Date)) %>%
+      dplyr::group_by(yearmonth)%>%
+      dplyr::select(-Date)%>%
+      dplyr::summarise(Precip = sum(Precip, na.rm = T),
+                       Tmin = mean(Tmin, na.rm = T),
+                       Tavg= mean(Tavg, na.rm = T),
+                       Tmax = mean(Tmax, na.rm = T))
+
+    # get periods
+    r.time<-zoo::as.Date(data_mon$yearmonth)
+
+    time_indices<-  periods <- list(
+      period1 = which(r.time > as.Date("1970-12-31") &
+                        r.time < as.Date("2000-02-01")),
+      period2 = which(r.time > as.Date("1990-12-31") &
+                        r.time < as.Date("2020-02-01")),
+      period3 = which(r.time > as.Date("2020-12-31") &
+                        r.time < as.Date("2050-02-01")),
+      period4 = which(r.time > as.Date("2069-12-31") &
+                        r.time < as.Date("2099-02-01"))
+    )
+
+    #assign periods
+    data_mon$Period<-NA
+    data_mon$Period[time_indices$period1]<-"1971-2000"
+    data_mon$Period[time_indices$period2]<-"1991-2020"
+    data_mon$Period[time_indices$period3]<-"2021-2050"
+    data_mon$Period[time_indices$period4]<-"2070-2099"
+
+
+    # calculate periods mean
+    data_seasonal<-data_mon%>%
+      dplyr::mutate(Date = zoo::as.Date(yearmonth))%>%
+      dplyr::mutate(MONTH = lubridate::month(Date))%>%
+      dplyr::group_by(Period, MONTH)%>%
+      dplyr::select(-yearmonth, -Date)%>%
+      dplyr::summarise(Precip = mean(Precip, na.rm = T),
+                       Tmin = mean(Tmin, na.rm = T),
+                       Tavg= mean(Tavg, na.rm = T),
+                       Tmax = mean(Tmax, na.rm = T))
+
+    # clean the data
+    data_seasonal<-na.omit(data_seasonal)
+
+
+    # get long term means
+    data_longterm<-sub_df%>%
+      dplyr::mutate(year = lubridate::year(Date)) %>%
+      dplyr::group_by(year)%>%
+      dplyr::select(-Date)%>%
+      dplyr::summarise(Precip = sum(Precip, na.rm = T),
+                       Tmin = mean(Tmin, na.rm = T),
+                       Tavg= mean(Tavg, na.rm = T),
+                       Tmax = mean(Tmax, na.rm = T))
+
+
+    # get periods
+    r.time<-data_longterm$year
+
+    time_indices<-  periods <- list(
+      period1 = which(r.time > 1970 &
+                        r.time < 2001),#Referenz
+      period2 = which(r.time > 1990 &
+                        r.time < 2021),
+      period3 = which(r.time > 2020 &
+                        r.time < 2051),
+      period4 = which(r.time > 2069&
+                        r.time < 2100)
+    )
+
+    #assign periods
+    data_longterm$Period<-NA
+    data_longterm$Period[time_indices$period1]<-"1971-2000"
+    data_longterm$Period[time_indices$period2]<-"1991-2020"
+    data_longterm$Period[time_indices$period3]<-"2021-2050"
+    data_longterm$Period[time_indices$period4]<-"2070-2099"
+
+    data_longterm<-data_longterm%>%
+      dplyr::group_by(Period)%>%
+      dplyr::select(-year)%>%
+      dplyr::summarise(Precip = mean(Precip, na.rm = T),
+                       Tmin = mean(Tmin, na.rm = T),
+                       Tavg= mean(Tavg, na.rm = T),
+                       Tmax = mean(Tmax, na.rm = T))
+
+
+
+    # clean the data
+    data_longterm<-na.omit(data_longterm)
+
+
+
+    # get P1
+    clima_diagramm_abs(data = data_seasonal%>% dplyr::filter(Period =="1971-2000"),
+                       temp_precip_mean = data_longterm[1,c(4,2)],
+                       run_id = run_id)
+
+
+    # get P2
+
+    # get P3
+
+    # get P4
+
+
+
+
+
+    ggplot2::ggplot(data = data_seasonal,
+                    mapping = ggplot2::aes(x = MONTH))+
+
+      ggplot2::geom_col(mapping = ggplot2::aes(
+        y = Precip))+
+
+      ggplot2::geom_line(mapping = ggplot2::aes(
+        y = Tmin))+
+
+      ggplot2::facet_grid(Period~corners)+
+
+      ggplot2::scale_y_continuous(breaks = seq(-10,50,5),
+                                  sec.axis = ggplot2::sec_axis(~.*100))
 
     # calculate statistics
 
